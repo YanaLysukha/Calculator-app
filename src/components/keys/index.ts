9ce0b9f3b9527d8a83fb2fd15f183f36017f7b56
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-11-22 15:14:54 +0100

    feat: implement operator switching

--- src/components/keys/index.ts
+++ src/components/keys/index.ts
@@ -5,28 +5,34 @@ type KeysProps = Omit<ElementProps<HTMLElement>, 'tag'>;
 
 const keysConfig = [
     { type: 'operator', label: 'AC', action: 'reset' },
-    { type: 'operator', label: '+/-', action: 'sign change' },
-    { type: 'operator', label: '%', action: 'percent' },
-    { type: 'operator', label: '÷', action: 'division' },
+    { type: 'operator', label: ' +/- ', action: 'sign change' },
+    { type: 'operator', label: ' % ', action: 'percent' },
+    { type: 'operator', label: ' ÷ ', action: 'division' },
     { type: 'number', label: '7' },
     { type: 'number', label: '8' },
     { type: 'number', label: '9' },
-    { type: 'operator', label: '*', action: 'multiplication' },
+    { type: 'operator', label: ' * ', action: 'multiplication' },
     { type: 'number', label: '4' },
     { type: 'number', label: '5' },
     { type: 'number', label: '6' },
-    { type: 'operator', label: '-', action: 'subtraction' },
+    { type: 'operator', label: ' - ', action: 'subtraction' },
     { type: 'number', label: '1' },
     { type: 'number', label: '2' },
     { type: 'number', label: '3' },
-    { type: 'operator', label: '+', action: 'addition' },
+    { type: 'operator', label: ' + ', action: 'addition' },
     { type: 'number', label: '0' },
     { type: 'operator', label: '.', action: 'decimal' },
     { type: 'operator', label: '=', action: 'calculation' },
 ];
 
+const operators = ['-', '+', '%', '÷', '*'];
+
 export default class Keys extends BaseElement<HTMLElement> {
     private button!: BaseElement<HTMLButtonElement>;
+
+    private currentSign!: string;
+
+    private previousSign!: string;
 
     constructor(props: KeysProps) {
         super({ tag: 'div', class: 'keys-container', ...props });
@@ -51,7 +57,6 @@ export default class Keys extends BaseElement<HTMLElement> {
 
     private onClickHandler = () => {
         this.node.addEventListener('click', (e) => {
-            // позже заменить
             const display = document.querySelector('.display-container') as HTMLElement;
             const displayedNumber = display?.textContent ?? '0';
 
@@ -59,13 +64,14 @@ export default class Keys extends BaseElement<HTMLElement> {
 
             if (target.tagName === 'BUTTON') {
                 const action = target.dataset.action;
-                const content = target.textContent;
+                this.currentSign = target.textContent ?? '';
+
                 if (!action) {
-                    display.textContent = content;
+                    display.textContent = this.currentSign;
                     if (displayedNumber && displayedNumber === '0') {
-                        display.textContent = content;
+                        display.textContent = this.currentSign;
                     } else {
-                        display.textContent = displayedNumber + content;
+                        display.textContent = displayedNumber + this.currentSign;
                     }
                 } else if (
                     action === 'division' ||
@@ -75,7 +81,14 @@ export default class Keys extends BaseElement<HTMLElement> {
                     action === 'percent'
                 ) {
                     if (action === 'subtraction' || display.textContent !== '') {
-                        display.textContent = displayedNumber + content;
+                        if (operators.includes(this.previousSign) && operators.includes(this.currentSign)) {
+                            // replace operators
+                            const begin = displayedNumber.slice(0, displayedNumber.length-1);
+                            display.textContent = '';
+                            display.textContent = begin + this.currentSign;
+                        } else {
+                            display.textContent = displayedNumber + this.currentSign;
+                        }
                     } else {
                         display.textContent = '';
                     }
@@ -83,6 +96,7 @@ export default class Keys extends BaseElement<HTMLElement> {
                 if (action === 'decimal') {
                     display.textContent = displayedNumber + '.';
                 }
+                this.previousSign = this.currentSign;
             }
         });
     };
