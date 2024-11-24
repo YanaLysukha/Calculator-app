836398c0530d0118bbfa6f7ec3784bc824d1322e
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-11-24 16:15:50 +0100

    refactor: move logic

--- src/components/calculator/index.ts
+++ src/components/calculator/index.ts
@@ -15,15 +15,113 @@ export default class Calculator {
 
     putNumber = (num: Numbers) => {
         console.log(num);
+        // const display = document.querySelector('.display-container') as HTMLElement;
+        // const displayedNumber = display?.textContent ?? '0';
+        // display.textContent = this.currentSign;
+        // if (displayedNumber && displayedNumber === '0') {
+        //     display.textContent = this.currentSign;
+        // } else {
+        //     display.textContent = displayedNumber + this.currentSign;
+        // }
     }
 
     putOperator = (operator: Operators) => {
         console.log(operator);
+        // const display = document.querySelector('.display-container') as HTMLElement;
+        // const displayedNumber = display?.textContent ?? '0';
+        // if (operators.includes(this.previousSign) && operators.includes(this.currentSign)) {
+        //     // replace operators
+        //     const begin = displayedNumber.slice(0, displayedNumber.length-1);
+        //     display.textContent = '';
+        //     display.textContent = begin + this.currentSign;
+        // } else {
+        //     display.textContent = displayedNumber + this.currentSign;
+        // }
+        // this.previousSign = this.currentSign;
+
+
+        // const display = document.querySelector('.display-container') as HTMLElement;
+        // const displayedNumber = display?.textContent ?? '0';
+    
+        // if (display.textContent !== '') {
+        //     if (operators.includes(this.previousSign) && operators.includes(this.currentSign)) {
+        //         // replace operators
+        //         const begin = displayedNumber.slice(0, displayedNumber.length-1);
+        //         display.textContent = '';
+        //         display.textContent = begin + this.currentSign;
+        //     } else {
+        //         display.textContent = displayedNumber + this.currentSign;
+        //     }
+        // } else {
+        //     display.textContent = '';
+        // }
+        // this.previousSign = this.currentSign;
+    }
+
+    putDecimal = () => {
+        console.log('.');
+        // const display = document.querySelector('.display-container') as HTMLElement;
+        // const displayedNumber = display?.textContent ?? '0';
+        // display.textContent = displayedNumber + '.';
     }
 
     getData = () => {
         return '1 + 2';
     }
+
+    calculate = () => {
+        console.log('calculate');
+        // const display = document.querySelector('.display-container') as HTMLElement;
+        // const displayedNumber = display?.textContent ?? '0';
+        // const expressionParts = displayedNumber.split(' ');
+        // display.textContent = this.calculateNew(...expressionParts).toString(); 
+    }
+
+    // calculateNew = (...params: string[]) => {
+    //     let result: number = 0;
+    //     let num1: string = '';
+    //     let num2: string;
+    //     let operator: string = '';
+    //     for (let i = 0; i < params.length; i += 2) {
+    //         if(!isNaN(Number(params[i]))) {
+    //             num1 = params[i];
+    //             console.log('num1', num1);
+    //         }
+    //         if (isNaN(Number(params[i + 1]))) {
+    //             operator = params[i + 1];
+    //             console.log('operator', params[i + 1]);
+    //         }
+    //         if (!isNaN(Number(params[i + 2]))) {
+    //             num2 = params[i + 2];
+    //             console.log('num2', num2);
+    //             const currentResult = this.calculate(num1, operator, num2);
+    //             result += currentResult;
+    //         }
+    //     } 
+    //     return result;
+    // }
+
+    // calculate = (num1: string, operator: string, num2: string) => {
+    //     let result = 0;
+
+    //     switch (operator) {
+    //         case '-':
+    //             result = parseFloat(num1) - parseFloat(num2);
+    //             break;
+    //         case '+':
+    //             result = parseFloat(num1) + parseFloat(num2);
+    //             break;
+    //         case '*':
+    //             result = parseFloat(num1) * parseFloat(num2);
+    //             break;
+    //         case '÷':
+    //             result = parseFloat(num1) / parseFloat(num2);
+    //         default:
+    //             console.log(`${result} - Such an operator does not exist!`);
+    //     }
+
+    //     return result;
+    // }
 }
 
 class Numbers {}
--- src/components/keys/index.ts
+++ src/components/keys/index.ts
@@ -4,15 +4,7 @@ import './style.scss';
 
 type KeysProps = Omit<ElementProps<HTMLElement>, 'tag'>;
 
-const operators = ['-', '+', '%', '÷', '*'];
-
 export default class Keys extends BaseElement<HTMLElement> {
-    private button!: BaseElement<HTMLButtonElement>;
-
-    private currentSign!: string;
-
-    private previousSign!: string;
-
     private calculator: Calculator;
 
     constructor(props: KeysProps) {
@@ -23,13 +15,13 @@ export default class Keys extends BaseElement<HTMLElement> {
 
     private createKeys = () => {
         this.keysConfig.forEach((key) => {
-            this.button = new BaseElement<HTMLButtonElement>({ tag: 'button' });
-            this.button.node.textContent = key.label;
-            this.node.append(this.button.node);
+            const button = new BaseElement<HTMLButtonElement>({ tag: 'button' });
+            button.node.textContent = key.label;
+            this.node.append(button.node);
             if (key.className) {
-                this.button.node.classList.add(key.className);
+                button.node.classList.add(key.className);
             }
-            this.button.node.addEventListener('click', key.handler);
+            button.node.addEventListener('click', key.handler);
         });
     };
 
@@ -42,127 +34,44 @@ export default class Keys extends BaseElement<HTMLElement> {
     }
 
     addNumber = () => {
-        const display = document.querySelector('.display-container') as HTMLElement;
-        const displayedNumber = display?.textContent ?? '0';
-        display.textContent = this.currentSign;
-        if (displayedNumber && displayedNumber === '0') {
-            display.textContent = this.currentSign;
-        } else {
-            display.textContent = displayedNumber + this.currentSign;
-        }
+        this.calculator.putNumber(1);
     }
 
     checkOperator = () => {
-        const display = document.querySelector('.display-container') as HTMLElement;
-        const displayedNumber = display?.textContent ?? '0';
-    
-        if (display.textContent !== '') {
-            if (operators.includes(this.previousSign) && operators.includes(this.currentSign)) {
-                // replace operators
-                const begin = displayedNumber.slice(0, displayedNumber.length-1);
-                display.textContent = '';
-                display.textContent = begin + this.currentSign;
-            } else {
-                display.textContent = displayedNumber + this.currentSign;
-            }
-        } else {
-            display.textContent = '';
-        }
-        this.previousSign = this.currentSign;
+        this.calculator.putOperator('+')
     }
 
     checkSubtraction = () => {
-        const display = document.querySelector('.display-container') as HTMLElement;
-        const displayedNumber = display?.textContent ?? '0';
-        if (operators.includes(this.previousSign) && operators.includes(this.currentSign)) {
-            // replace operators
-            const begin = displayedNumber.slice(0, displayedNumber.length-1);
-            display.textContent = '';
-            display.textContent = begin + this.currentSign;
-        } else {
-            display.textContent = displayedNumber + this.currentSign;
-        }
-        this.previousSign = this.currentSign;
+        this.calculator.putOperator('-');
     }
 
     addDecimal = () => {
-        const display = document.querySelector('.display-container') as HTMLElement;
-        const displayedNumber = display?.textContent ?? '0';
-        display.textContent = displayedNumber + '.';
+        this.calculator.putDecimal();
     }
 
     calculateResult = () => {
-        const display = document.querySelector('.display-container') as HTMLElement;
-        const displayedNumber = display?.textContent ?? '0';
-        const expressionParts = displayedNumber.split(' ');
-        display.textContent = this.calculateNew(...expressionParts).toString(); 
-    }
-
-    calculateNew = (...params: string[]) => {
-        let result: number = 0;
-        let num1: string = '';
-        let num2: string;
-        let operator: string = '';
-        for (let i = 0; i < params.length; i += 2) {
-            if(!isNaN(Number(params[i]))) {
-                num1 = params[i];
-                console.log('num1', num1);
-            }
-            if (isNaN(Number(params[i + 1]))) {
-                operator = params[i + 1];
-                console.log('operator', params[i + 1]);
-            }
-            if (!isNaN(Number(params[i + 2]))) {
-                num2 = params[i + 2];
-                console.log('num2', num2);
-                const currentResult = this.calculate(num1, operator, num2);
-                result += currentResult;
-            }
-        } 
-        return result;
-    }
-
-    calculate = (num1: string, operator: string, num2: string) => {
-        let result = 0;
-
-        switch (operator) {
-            case '-':
-                result = parseFloat(num1) - parseFloat(num2);
-                break;
-            case '+':
-                result = parseFloat(num1) + parseFloat(num2);
-                break;
-            case '*':
-                result = parseFloat(num1) * parseFloat(num2);
-                break;
-            case '÷':
-                result = parseFloat(num1) / parseFloat(num2);
-            default:
-                console.log(`${result} - Such an operator does not exist!`);
-        }
-
-        return result;
+        this.calculator.calculate();
     }
 
     keysConfig = [
-        { label: 'AC', handler: this.reset, className: 'key-operator' },
-        { label: ' +/- ', handler: this.toggleSigns, className: 'key-operator' },
-        { label: ' % ', handler: this.checkOperator, className: 'key-operator' },
-        { label: ' ÷ ', handler: this.checkOperator, className: 'key-operator' },
+        { label: 'AC', handler: this.reset },
+        { label: ' +/- ', handler: this.toggleSigns },
+        { label: ' % ', handler: this.checkOperator },
+        { label: ' ÷ ', handler: this.checkOperator },
         { label: '7', handler: this.addNumber },
         { label: '8', handler: this.addNumber },
         { label: '9', handler: this.addNumber },
-        { label: ' * ', handler: this.checkOperator, className: 'key-operator' },
+        { label: ' * ', handler: this.checkOperator },
         { label: '4', handler: this.addNumber },
         { label: '5', handler: this.addNumber },
         { label: '6', handler: this.addNumber },
-        { label: ' - ', handler: this.checkSubtraction, className: 'key-operator' },
+        { label: ' - ', handler: this.checkSubtraction },
         { label: '1', handler: this.addNumber },
         { label: '2', handler: this.addNumber },
         { label: '3', handler: this.addNumber },
-        { label: ' + ', handler: this.checkOperator, className: 'key-operator' },
+        { label: ' + ', handler: this.checkOperator },
         { label: '0', handler: this.addNumber, className: 'zero-number' },
-        { label: '.', handler: this.addDecimal, className: 'key-operator' },
-        { label: '=', handler: this.calculateResult, className: 'key-operator' },
+        { label: '.', handler: this.addDecimal },
+        { label: '=', handler: this.calculateResult },
     ];
 }
