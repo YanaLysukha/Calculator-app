d011419a10d38486fa092e05c56973e5cdeee9fa
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-11-26 22:56:34 +0100

    feat: add styles to theme switcher button

--- src/components/ThemeManager/index.ts
+++ src/components/ThemeManager/index.ts
@@ -1,4 +1,5 @@
 import BaseElement from '../base-component';
+import './style.scss';
 
 export default class ThemeManager extends BaseElement<HTMLElement>{
     static #instance: ThemeManager;
@@ -6,9 +7,18 @@ export default class ThemeManager extends BaseElement<HTMLElement>{
     private subscribers: (() => void)[];
 
     private constructor() {
-        super({ tag: 'button', class: 'theme-manager-button'});
-        this.node.onclick = this.toggleTheme;
+        super({ tag: 'div', class: 'theme-manager-wrapper'});
+        this.createThemeSwitcher();
         this.subscribers = [];
+    }
+
+    private createThemeSwitcher = () => {
+        const switcherLabel = new BaseElement<HTMLLabelElement>({ tag: 'label', class: 'switch' });
+        this.node.append(switcherLabel.node);
+        const inputThemeSwitcher = new BaseElement<HTMLInputElement>({ tag: 'input', type: 'checkbox', id: 'theme-switcher' });
+        const stylesSlider = new BaseElement<HTMLSpanElement>({ tag: 'span', class: 'slider', onclick: this.toggleTheme });
+        switcherLabel.node.append(inputThemeSwitcher.node);
+        switcherLabel.node.append(stylesSlider.node);
     }
 
     public static getInstance(): ThemeManager {
